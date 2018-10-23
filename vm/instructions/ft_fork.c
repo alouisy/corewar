81bd5bf25ecf3ceea4b454ef591b60223d3ba2d5
jgroc-de <jgroc-de@student-42.fr>
2018-10-23 17:34:00 +0200

    live ok

--- vm/instructions/ft_fork.c
+++ vm/instructions/ft_fork.c
@@ -7,9 +7,12 @@
 
 void	ft_fork(t_pvm *pvm, t_process *process)
 {
+	int value;
 	int		address;
 	t_list	*node;
 
+	value = 0;
+	(void)node;
 	address = pvm->memory[process->param[0] + process->param[1]];
 	process->r[process->param[2]] = address;
 	if (value == 0)
