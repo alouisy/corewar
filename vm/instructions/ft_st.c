b307131e3317c9ab08f8dcc10fd9e70b7c72b729
jgroc-de <jgroc-de@student-42.fr>
2018-10-23 17:49:00 +0200

    clean

--- vm/instructions/ft_st.c
+++ vm/instructions/ft_st.c
@@ -6,7 +6,7 @@
 /*   By: jgroc-de <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/18 17:01:23 by jgroc-de          #+#    #+#             */
-/*   Updated: 2018/10/20 17:59:14 by jgroc-de         ###   ########.fr       */
+/*   Updated: 2018/10/23 17:46:58 by jgroc-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,9 @@
 
 void	ft_st(UNUSED t_pvm *pvm, t_process *process)
 {
-	process->param[1] = process->r[process->param[0]] % IDX_MOD;
+	int	value;
+
+	value = process->pc + (process->r[process->param[0]] % IDX_MOD);
 	if (process->param[0] == 0)
 		process->carry = 1;
 	else
