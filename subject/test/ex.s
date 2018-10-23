c048b03042dcb99c7cc2ef94cfa921da62193a43
jgroc-de <jgroc-de@student-42.fr>
2018-10-23 14:55:27 +0200

    test

--- subject/test/ex.s
+++ subject/test/ex.s
@@ -2,8 +2,15 @@
 .comment "testing"
 
 l2:		sti r1, %:live, %1
-		and r1, %0, r1
+		and %1, %1, r6
+		and r1, r1, r0
+		and r1, r1, r2
+		and r1, r1, r3
+		and r1, r1, r4
+		and r1, %0, r5
 
 live:	live %1
-		fork %-513
+		fork %:write
 		zjmp %:live
+write:	sti r6, %20, %0
+		sti r1, %17, %0
