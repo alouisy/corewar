2776c6aadf012bf3b6d6733a018d8e7d398f86cc
jgroc-de <jgroc-de@student-42.fr>
2018-10-23 17:51:32 +0200

    encours de st

--- vm/instructions/ft_st.c
+++ vm/instructions/ft_st.c
@@ -6,7 +6,7 @@
 /*   By: jgroc-de <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/18 17:01:23 by jgroc-de          #+#    #+#             */
-/*   Updated: 2018/10/23 17:46:58 by jgroc-de         ###   ########.fr       */
+/*   Updated: 2018/10/23 17:51:17 by jgroc-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,34 @@
 ** direct store
 */
 
-void	ft_st(UNUSED t_pvm *pvm, t_process *process)
+void	ft_st(t_pvm *pvm, t_process *process)
 {
-	int	value;
+	int		value;
+	int		address;
 
-	value = process->pc + (process->r[process->param[0]] % IDX_MOD);
-	if (process->param[0] == 0)
+	value = process->r[process->param[0] - 1];
+	address = process->pc + ((process->param[1] + process->param[2]) % IDX_MOD);
+	if (address < 0)
+		address += MEM_SIZE;
+	pvm->memory[(address + 3) % MEM_SIZE] = value;
+	pvm->memory[(address + 2) % MEM_SIZE] = (value >> 8);
+	pvm->memory[(address + 1) % MEM_SIZE] = (value >> 16);
+	pvm->memory[(address + 0) % MEM_SIZE] = (value >> 24);
+	if (value == 0)
 		process->carry = 1;
 	else
 		process->carry = 0;
 }
+
+/*
+void	ft_sti(t_pvm *pvm, t_process *process)
+{
+	int	value;
+
+	value = (pvm->memory[process->param[0] + process->param[1]]) % IDX_MOD;
+	process->r[process->param[2]] = value;
+	if (value == 0)
+		process->carry = 1;
+	else
+		process->carry = 0;
+}*/
