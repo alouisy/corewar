97549eccd7d5b1047ae4ba21bb5e81cea2c02453
jgroc-de <jgroc-de@student-42.fr>
2018-11-16 16:13:34 +0100

    fail/ok usr la partie inf du tableau op.c

--- vm/op.c
+++ vm/op.c
@@ -6,7 +6,7 @@
 /*   By: zaz <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/10/04 11:43:01 by zaz               #+#    #+#             */
-/*   Updated: 2018/11/16 15:56:10 by jgroc-de         ###   ########.fr       */
+/*   Updated: 2018/11/16 16:13:02 by jgroc-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,15 +33,15 @@ t_op	g_op_tab[17] =
 		"ou  (or   r1, r2, r3   r1 | r2 -> r3", 1, 0},
 	{"xor", 3, {T_REG | T_IND | T_DIR, T_REG | T_IND | T_DIR, T_REG}, 8, 6,//
 		"ou (xor  r1, r2, r3   r1^r2 -> r3", 1, 0},
-	{"zjmp", 1, {T_DIR}, 9, 20, "jump if zero", 0, 1}, //
-	{"ldi", 3, {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG}, 10, 25, //
+	{"zjmp", 1, {T_DIR}, 9, 20, "jump if zero", 0, 1}, //ok?
+	{"ldi", 3, {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG}, 10, 25, //fail
 		"load index", 1, 1},
-	{"sti", 3, {T_REG, T_REG | T_DIR | T_IND, T_DIR | T_REG}, 11, 25, //
+	{"sti", 3, {T_REG, T_REG | T_DIR | T_IND, T_DIR | T_REG}, 11, 25, //fail
 		"store index", 1, 1},
-	{"fork", 1, {T_DIR}, 12, 800, "fork", 0, 1}, //
-	{"lld", 2, {T_DIR | T_IND, T_REG}, 13, 10, "long load", 1, 0}, //
-	{"lldi", 3, {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG}, 14, 50, //
+	{"fork", 1, {T_DIR}, 12, 800, "fork", 0, 1}, //fork
+	{"lld", 2, {T_DIR | T_IND, T_REG}, 13, 10, "long load", 1, 0}, //ok
+	{"lldi", 3, {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG}, 14, 50, //fail
 		"long load index", 1, 1},
-	{"lfork", 1, {T_DIR}, 15, 1000, "long fork", 0, 1},//
-	{"aff", 1, {T_REG}, 16, 2, "aff", 1, 0} //
+	{"lfork", 1, {T_DIR}, 15, 1000, "long fork", 0, 1},//ok gg
+	{"aff", 1, {T_REG}, 16, 2, "aff", 1, 0} //ok gg
 };
