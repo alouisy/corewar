1035a42ed4e25e2108687609e394a75ff156cd26
jgroc-de <jgroc-de@student-42.fr>
2018-11-17 20:30:55 +0100

    process->pc est signed

--- vm/instructions/write_in_memory.c
+++ vm/instructions/write_in_memory.c
@@ -6,7 +6,7 @@
 /*   By: jgroc-de <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/18 17:01:23 by jgroc-de          #+#    #+#             */
-/*   Updated: 2018/11/17 16:11:23 by jgroc-de         ###   ########.fr       */
+/*   Updated: 2018/11/17 20:03:14 by jgroc-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ void	write_in_memory(t_pvm *pvm, t_process *process, int value, int value2)
 	while (i < 4)
 	{
 		tmp = (address - i) % MEM_SIZE;
+		if (tmp < 0)
+			tmp %= MEM_SIZE;
 		pvm->memory[tmp] = value >> (i * 8);
 		pvm->mem_color[tmp] = color;
 		i++;
